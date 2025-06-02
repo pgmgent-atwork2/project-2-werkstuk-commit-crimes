4913328a74329de8d3ba8295005d485845dd2bce
«liamsche» <liam.linck@gmail.com>
2025-06-02 16:50:15 +0200

    fixed delete quiz and added add quiz function

--- src/controllers/QuizController.js
+++ src/controllers/QuizController.js
@@ -1,10 +1,10 @@
 import QuizItem from "../lib/models/QuizItem.js";
 
 export const index = (req, res) => {
-    res.render('layout', {
-        title: "Make It Happen",
-        body: "index",
-    });
+  res.render("layout", {
+    title: "Make It Happen",
+    body: "index",
+  });
 };
 
 export const getAllQuizes = async (req, res) => {
@@ -18,18 +18,41 @@ export const getAllQuizes = async (req, res) => {
   }
 };
 
-
 export async function updateQuiz(req, res) {
   try {
     const { id } = req.params;
     const { title } = req.body;
 
-    const updatedQuiz = await QuizItem.query()
-      .patchAndFetchById(id, { title });
+    const updatedQuiz = await QuizItem.query().patchAndFetchById(id, { title });
 
     res.status(200).json(updatedQuiz);
   } catch (err) {
     console.error("Fout bij updaten quiz:", err);
     res.status(500).json({ error: "Interne serverfout" });
+  }
+}
+
+export async function createQuiz(req, res) {
+  try {
+    const { title, language } = req.body;
+    if (!title || !language) {
+      return res.status(400).json();
+    }
+    const newQuiz = await QuizItem.query().insert({ title, language });
+    res.status(201).json(newQuiz);
+  } catch (error) {
+    console.error("Fout bij aanmaken quiz:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+export async function deleteQuiz(req, res) {
+  try {
+    const { id } = req.params;
+    await QuizItem.query().deleteById(id);
+    res.json({ success: true });
+  } catch (error) {
+    console.error("Fout bij verwijderen quiz:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 }
--- src/routes/quizRoutes.js
+++ src/routes/quizRoutes.js
@@ -1,8 +1,10 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import * as quizController from "../controllers/QuizController.js"
+import * as quizController from "../controllers/QuizController.js";
 
 router.get("/", quizController.getAllQuizes);
+router.post("/", quizController.createQuiz);
 router.patch("/:id", quizController.updateQuiz);
+router.delete("/:id", quizController.deleteQuiz);
 
 export default router;
