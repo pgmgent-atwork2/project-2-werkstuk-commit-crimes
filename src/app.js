1d70b9b6a61726ae47667f452516ffef89292c1e
robbeornelis <robbe.ornelis@student.arteveldehs.be>
2025-06-06 10:10:01 +0200

    added some codes to the quiz-editor html file, added some margin to the correct container in the quiz-editor.css file and also saved in the app.js file so now that file also wants to be pushed even though I didn't change anything

--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
-import express from 'express';
-import cors from 'cors';
-import answerRoutes from './routes/answerRoutes.js';
-import questionRoutes from './routes/questionRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import sessionRoutes from './routes/sessionRoutes.js';
-import quizRoutes from './routes/quizRoutes.js';
+import express from "express";
+import cors from "cors";
+import answerRoutes from "./routes/answerRoutes.js";
+import questionRoutes from "./routes/questionRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import sessionRoutes from "./routes/sessionRoutes.js";
+import quizRoutes from "./routes/quizRoutes.js";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/images', express.static('public/images'))
-app.use('/api/answers', answerRoutes);
-app.use('/api/questions', questionRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/sessions', sessionRoutes);
-app.use('/api/quiz', quizRoutes);
+app.use("/images", express.static("public/images"));
+app.use("/api/answers", answerRoutes);
+app.use("/api/questions", questionRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/sessions", sessionRoutes);
+app.use("/api/quiz", quizRoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
