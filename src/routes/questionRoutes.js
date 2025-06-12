2db66b46ef35bcc984feac2ad747f8697fc35edc
«liamsche» <liam.linck@gmail.com>
2025-06-12 11:18:20 +0200

    added language selector

--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import * as questionController from '../controllers/QuestionsController.js';
-import upload from '../middleware/upload-images.js';
 
 const router = express.Router();
 
@@ -9,6 +8,6 @@ router.get('/', questionController.getAllQuestions);
 router.patch('/:id', questionController.updateQuestion);
 router.delete('/:id', questionController.deleteQuestion);
 
-router.post('/', upload.single('image'), questionController.postQuestion);
+router.post('/', questionController.postQuestion);
 
 export default router;
