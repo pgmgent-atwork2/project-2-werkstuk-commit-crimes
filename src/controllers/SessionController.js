2a0b9b8267603f0a7e69ceb83b15aea407e7b478
Nelson-Mart <nelsonmartinel2@gmail.com>
2025-06-13 16:23:05 +0200

    fixed sessions

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -53,3 +53,26 @@ export async function getQuizWithQuestions(req, res) {
     res.status(500).json({ error: "Fout bij ophalen van quizvragen" });
   }
 }
+
+
+export const checkExpiredSessions = async () => {
+  try {
+    const expiredSessionTimer = new Date(Date.now() - 30 * 60 * 1000).toISOString();
+    
+    const expiredSessions = await SessionItem.query()
+      .where('created_at', '<', expiredSessionTimer)
+      .where('second_try', false);
+
+    for (const session of expiredSessions) {
+      await SessionItem.query()
+        .findById(session.id)
+        .patch({ second_try: true });
+      console.log(`Updated session ${session.id} to second_try=true`);
+    }
+  } catch (error) {
+    console.error('Error checking expired sessions:', error);
+  }
+};
+
+// checks if minutes are over
+setInterval(checkExpiredSessions, 60 * 1000);
