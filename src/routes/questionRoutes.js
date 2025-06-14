d631e571df203c27d70a24cf4837d5b3143e7172
Nelson-Mart <nelsonmartinel2@gmail.com>
2025-06-14 15:17:51 +0200

    youre now able to add images but make sure theyre not too large files

--- src/routes/questionRoutes.js
+++ src/routes/questionRoutes.js
@@ -4,6 +4,7 @@ import * as questionController from "../controllers/QuestionsController.js";
 const router = express.Router();
 
 router.get("/", questionController.getAllQuestions);
+router.post("/", questionController.postQuestionWithImage);
 
 router.patch("/:id", questionController.updateQuestion);
 router.delete("/:id", questionController.deleteQuestion);
